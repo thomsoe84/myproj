b5eda404bb60100d1252db88b6829efe95fad003
thomsoe84 <56757373+thomsoe84@users.noreply.github.com>
2019-10-22 02:21:45 +0700

    Update test_hitung.py

--- test/test_hitung.py
+++ test/test_hitung.py
@@ -1,1 +1,10 @@
+import unittest
+import hitung
 
+class TestHitung(unittest.TestCase):
+    def test_kali_dua(self):
+        self.assertTrue(hitung.kali_dua(5) == 10)
+        self.assertTrue(hitung.kali_dua(6) == 12)
+
+if __name__ == '__main__':
+    unittest.main()
